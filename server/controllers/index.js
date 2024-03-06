ec9d49426d47347baac2974366c74ddbfd6201d9
Conor Race (RIT Student) <ctr9664@rit.edu>
2024-03-06 14:39:25 -0500

    Worked on views and templates in-class demo.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,27 +3,45 @@
 let name = 'unknown';
 
 const hostIndex = (req, res) => {
-
+  return res.render('index', { 
+    title: 'Home Page',
+    pageName: 'Home Page',
+    userName: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
+  const users = ['Conor', 'Liam', 'Collin'];
+  const filtered = users.filter(x => x === req.query.name);
 
+  return res.render('page1', { 
+    title: 'Page 1',
+    users: filtered,
+   });
 };
 
 const hostPage2 = (req, res) => {
-
+  return res.render('page2');
 };
 
 const getName = (req, res) => {
-
+  return res.json({ name });
 };
 
 const setName = (req, res) => {
+  if (!req.body.firstname || !req.body.lastname) {
+    return res.status(400).json({
+      error: 'Missing firstname and/or lastname parameters',
+      id: 'setNameMissingParams',
+    });
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  return res.json({ name });
 };
 
 const notFound = (req, res) => {
-
+  return res.status(404).render('notFound', { url: req.url });
 };
 
 module.exports = {
